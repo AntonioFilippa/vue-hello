86867bb948af61e8f0e3cd9761edb13af4186287
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-05-20 17:17:46 +0200

    first commit

---
+++ bonus/js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+//COLLEGO VUE.JS ALL ID "APP"
+
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Questo è il mio messaggio',
+      imageUrl: 'https://m.media-amazon.com/images/I/51m+zpGqxFL._AC_.jpg://static.wikia.nocookie.net/harrypotter/images/0/09/Case_di_Hogwarts.jpg/revision/latest?cb=20210825140930&path-prefix=it'
+    }
+  })
---
+++ vue-hello/js/main.js
@@ -0,0 +1,9 @@
+'use strict'
+//COLLEGO VUE.JS ALL ID "APP"
+
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Questo è il mio messaggio'
+    }
+  })
